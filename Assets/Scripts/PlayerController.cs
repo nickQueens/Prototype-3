f4c0241561d6bf0adb108d2d900afbe758e8e099
nickQueens <146036623+nickQueens@users.noreply.github.com>
2023-11-09 00:00:40 +0000

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float jumpForce;
+    public float gravityModifier;
+    private bool isOnGround = true;
+    private Rigidbody playerRb;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
