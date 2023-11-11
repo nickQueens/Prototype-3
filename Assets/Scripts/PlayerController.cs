3071c2a7846899616a1b2af8e94907cbebc05629
nickQueens <146036623+nickQueens@users.noreply.github.com>
2023-11-11 18:02:37 +0000

    Jumping bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -46,7 +46,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
             dirtParticle.Play();
